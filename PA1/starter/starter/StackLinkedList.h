48806e39c1dbf9af602bda4215e63ac327c5dd16
CarterCummings <cartercummings1@gmail.com>
2024-02-05 13:32:27 -0600

    not 100% broken

--- PA1/starter/starter/StackLinkedList.h
+++ PA1/starter/starter/StackLinkedList.h
@@ -38,44 +38,115 @@ public:
 // Your implementation here
 template <typename T>
 StackLinkedList<T>::StackLinkedList(){
+    head = nullptr;
+    length = 0;
 }
 
 template <typename T>
 StackLinkedList<T>::~StackLinkedList(){
+    while (!isEmpty()) {
+            Node<T>* temp = head;
+            head = head->next;
+            delete temp;
+        }
 }
 
 template <typename T>
 StackLinkedList<T>::StackLinkedList(const StackLinkedList& other) {
+        head = nullptr;
+
+        Node<T>* currentOther = other.head;
+        Node<T>* lastNewNode = nullptr;
+
+        while (currentOther != nullptr) {
+            Node<T>* newNode = new Node<T>{currentOther->data};
+;
+            if (lastNewNode == nullptr) {
+                head = newNode;
+            } else {
+                lastNewNode->next = newNode;
+            }
+
+            lastNewNode = newNode;
+            currentOther = currentOther->next;
+        }
 }
 
 template <typename T>
 StackLinkedList<T>& StackLinkedList<T>::operator=(const StackLinkedList& other) {
-    return *this;
+        if (this != &other) {
+            while (!isEmpty()) {
+                Node<T>* temp = head;
+                head = head->next;
+                delete temp;
+            }
+
+            head = nullptr;
+
+            Node<T>* currentOther = other.head;
+            Node<T>* lastNewNode = nullptr;
+
+            while (currentOther != nullptr) {
+                Node<T>* newNode = new Node<T>{currentOther->data};
+;
+                if (lastNewNode == nullptr) {
+                    head = newNode;
+                } else {
+                    lastNewNode->next = newNode;
+                }
+
+                lastNewNode = newNode;
+                currentOther = currentOther->next;
+            }
+        }
+        return *this;
 }
 
 template <typename T>
 bool StackLinkedList<T>::isEmpty(){
-    return true;
+    return head == nullptr;
 }
 
 template <typename T>
 int StackLinkedList<T>::size(){
-    return -1;
+        int count = 0;
+        Node<T>* current = head;
+        while (current != nullptr) {
+            count++;
+            current = current->next;
+        }
+        return count;
 }
 
 template <typename T>
 T& StackLinkedList<T>::top(){
-    static int temp = -1;
-    return temp;
+        if (isEmpty()) {
+            throw std::out_of_range("Stack empty");
+        }
+
+        return head->data;
 }
 
 template <typename T>
 T StackLinkedList<T>::pop(){
-    return -1;
+        if (isEmpty()) {
+            throw std::out_of_range("Stack underflow");
+        }
+
+        T e = head->data;
+        Node<T>* temp = head;
+        head = head->next;
+        delete temp;
+        length--;
+        return e;
 }
 
 template <typename T>
 void StackLinkedList<T>::push(const T& e){
+    Node<T>* newNode = new Node<T>{e};
+        head->next = head;
+        head = newNode;
+        length++;
 }
 
 
