066cf249776d270f939a6be6df9b30c8dd7ce3b0
CarterCummings <cartercummings1@gmail.com>
2024-02-05 11:04:37 -0600

    20/50

--- PA1/starter/starter/StackArrayLinear.h
+++ PA1/starter/starter/StackArrayLinear.h
@@ -33,44 +33,96 @@ public:
 // Your implementation here
 template <typename T>
 StackArrayLinear<T>::StackArrayLinear(){
+    // Setting 1 to the be starting stack size 
+    data = new T[1];
+    length = 1;
+    topIndex = 0;
 }
 
 template <typename T>
 StackArrayLinear<T>::~StackArrayLinear(){
+    delete[] data;
 }
 
 template <typename T>
 StackArrayLinear<T>::StackArrayLinear(const StackArrayLinear& other) {
+    length = other.length;
+    data = new T[length];
+    topIndex = other.topIndex;
+
+    for (int i = 0; i <= topIndex; ++i) {
+        data[i] = other.data[i];
+    }
 }
 
 template <typename T>
 StackArrayLinear<T>& StackArrayLinear<T>::operator=(const StackArrayLinear& other) {
+    if (this != &other) {
+            delete[] data;
+
+            length = other.length;
+            topIndex = other.topIndex;
+
+            data = new T[length];
+            for (int i = 0; i <= topIndex; ++i) {
+                data[i] = other.data[i];
+            }
+        }
     return *this;
 }
 
 template <typename T>
 bool StackArrayLinear<T>::isEmpty(){
-    return true;
+    return topIndex == 0;
 }
 
 template <typename T>
 int StackArrayLinear<T>::size(){
-    return -1;
+    return topIndex;
 }
 
 template <typename T>
 T& StackArrayLinear<T>::top(){
     static int temp = -1;
-    return temp;
+
+    if (isEmpty()) { 
+        throw std::out_of_range("its empty");
+    }
+    return data[topIndex];
+
+    //return temp;
 }
 
 template <typename T>
 T StackArrayLinear<T>::pop(){
-    return -1;
+    if (isEmpty()) {
+            throw std::out_of_range("its empty");
+            return data[topIndex];
+        }
+
+    T e = data[topIndex--];
+
+    return e;
 }
 
 template <typename T>
 void StackArrayLinear<T>::push(const T& e){
+    if (topIndex == length - 1) {
+            // If the stack is full, double its capacity
+            T* newData = new T[length + 10];
+
+            // Copy existing elements to the new array
+            for (int i = 0; i <= topIndex; ++i) {
+                newData[i] = data[i];
+            }
+
+            // Update length and swap the arrays
+            length = length + 10;
+            delete[] data;
+            data = newData;
+        }
+
+        data[++topIndex] = e;
 }
 
 #endif
