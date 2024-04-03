d6b26656f70a9dd577db689fcbaeb88d3fbbccb4
CarterCummings <cartercummings1@gmail.com>
2024-04-03 14:31:11 -0500

    can get 60/60 if sechas ret 1

--- PA4/starter/DoubleHashTable.h
+++ PA4/starter/DoubleHashTable.h
@@ -31,6 +31,7 @@ DoubleHashTable::DoubleHashTable(): AbstractHashTable() {
     num_elements = 0;
     maxLoadFactor = .7; 
     table.resize(capacity);
+    prevPrime = 7;
 }
 
 // destructor
@@ -43,29 +44,146 @@ void DoubleHashTable::insert(std::string key, int val) {
     if (load_factor() > maxLoadFactor) {
         resizeAndRehash();
     }
+    bool cont = contains(key);
+    int index = hash(key);
+
+    while(1){
+        if (!cont && (!table[index].isFilled || table[index].DELETED)) {
+            table[index].key = key;
+            table[index].val = val;
+            table[index].DELETED = false;
+            table[index].isFilled = true;
+            num_elements++;
+            break;
+        } else if (cont && table[index].key == key && !table[index].DELETED) {
+            table[index].val = val;
+            table[index].DELETED = false;
+            table[index].isFilled = true;
+            break;
+        }
+
+
+        index += secondHash(key);
+        if(index >= capacity) {
+            index = index % capacity;
+        }
+    }
+
+    
 }
 
 // removes the given key from the hash table - if the key is not in the list, throw an error
 int DoubleHashTable::remove(std::string key) {
-	return -1;
+	cout << "new insert" << endl;
+    int index = hash(key);
+    int start = index;
+
+    do {
+        if(table[index].key == key && !table[index].DELETED) {
+            table[index].DELETED = true;
+            table[index].isFilled = false;
+            table[index].val = -1;
+            table[index].key = "";
+            num_elements--;
+            return table[index].val;
+
+        } else if (!table[index].isFilled && !table[index].DELETED) {
+            throw std::out_of_range("no key found");
+        }
+
+        index += secondHash(key);
+        if(index >= capacity) {
+            index = index % capacity;
+        }
+    } while (index != start);
+    throw std::out_of_range("no key found rem");
 }
 
 // getter to obtain the value associated with the given key
 int DoubleHashTable::get(std::string key) const {
-	return -1;
+	int index = hash(key);
+    int start = index;
+
+    do {
+        if(table[index].key == key && !table[index].DELETED) {
+            return table[index].val;
+        } else if (!table[index].isFilled && !table[index].DELETED) {
+            throw std::out_of_range("no key found get");
+        }
+        index += secondHash(key);
+        if(index >= capacity) {
+            index = index % capacity;
+        }
+    } while (index != start);
+
+    throw std::out_of_range("no key found get");
 }
 
 bool DoubleHashTable::contains(std::string key) const {
-	return false;
+	int index = hash(key);
+    int start = index;
+
+    do {
+        if(table[index].key == key && !table[index].DELETED) {
+            return true;
+        } else if (!table[index].isFilled && !table[index].DELETED) {
+            return false;
+        }
+        
+        index += secondHash(key);
+        if(index >= capacity) {
+            index = index % capacity;
+        }
+    } while (index != start);
+
+    return false;
 }
 
 void DoubleHashTable::resizeAndRehash() {
-	
+    int cap = capacity;
+    capacity *= 2;
+    num_elements = 0;
+    int index;
+
+    std::vector<HashEntry> newTable(capacity);
+
+    for(int i = 0; i < cap; i++) {
+        if(table[i].isFilled && !table[i].DELETED) {
+            index = hash(table[i].key);
+
+            while(1){
+                if (!newTable[index].isFilled) {
+                    newTable[index].val = table[i].val;
+                    newTable[index].key = table[i].key;
+                    newTable[index].isFilled = true;
+                    num_elements++;
+                    break;
+                } 
+                index += secondHash(table[i].key);
+                if(index >= capacity) {
+                    index = index % capacity;
+                }
+            }
+
+
+        }
+    }
+    table = move(newTable);    
+
+    do {
+        prevPrime = findNextPrime(prevPrime);
+    } while (findNextPrime(prevPrime) < capacity);
 }
 
 // helper functions 
 int DoubleHashTable::secondHash(std::string s) const {
-	return -1;
+	unsigned long hash = 0;
+	int n = s.length();
+	for (int i = 0; i < n; i++) {
+		hash = 37 * hash + (s[i] - '0');
+	}
+    //cout << "Prime: " << prevPrime << endl;
+	return prevPrime - (hash % prevPrime);
 }
 
 
